cd9cb4e959247947ed3299f7ac3e345e87d21668
mainbosskay <dadakehinde80@yahoo.com>
2023-10-10 06:50:59 -0700

    Printing alphabeths in lowercas

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing lowercase alphabeth
+ * Return: is void
+ */
+
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a' ; k <= 'z' ; k++)
+		_putchar(k);
+	_putchar('\n');
+}
