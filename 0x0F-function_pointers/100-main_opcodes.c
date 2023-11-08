611b7788f0e38f9c779597da8cffca5c7843c66f
mainbosskay <dadakehinde80@yahoo.com>
2023-11-08 04:21:36 -0800

    Program that prints the opcodes of its own manin function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Printing the opcodes of its main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: it is 0
+ */
+
+int main(int argc, char *argv[])
+{
+	char *array;
+	int k;
+	int size;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	size = atoi(argv[1]);
+	if (size < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	array = (char *)main;
+	for (k = 0 ; k < size ; k++)
+	{
+		if (k == size - 1)
+		{
+			printf("%02hhx\n", array[k]);
+			break;
+		}
+		printf("%02hhx", array[k]);
+	}
+	return (0);
+}
