a6d6679da8c5b3762873dd0102e6dd9e528c8e5d
mainbosskay <dadakehinde80@yahoo.com>
2024-03-20 10:13:36 -0700

    Python program that creates a function def island_perimeter(grid): that returns the perimeter of the island described in grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,7 +14,7 @@ def island_perimeter(grid):
             if (grid[k][t] == 1):
                 ledg = 1 if t == 0 or grid[k][t - 1] == 0 else 0
                 tedg = 1 if k == 0 or grid[k - 1][t] == 0 else 0
-                redg = 1 if t == colms - 1 or grid[k][t + 1] == 0 else 0
+                redg = 1 if t == colums - 1 or grid[k][t + 1] == 0 else 0
                 bedg = 1 if k == rows - 1 or grid[k + 1][t] == 0 else 0
                 permeta += (ledg + tedg + redg + bedg)
     return permeta
